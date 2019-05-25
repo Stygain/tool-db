00267465c46c000fad6a86d9798d750be7c9f54a
Adam Barton <adamb6102@gmail.com>
2019-05-25 16:17:39 -0700

    Initial commit of web application, structure present for future development.

---
+++ server.js
@@ -0,0 +1,100 @@
+var express = require('express');
+var server = express();
+var fs = require('fs');
+var http = require('http');
+var path = require('path');
+var exphbs = require('express-handlebars');
+var hbs = require('handlebars');
+// var bootstrap = require('bootstrap');
+
+var port = process.env.PORT || 3000;
+
+server.engine('handlebars', exphbs({defaultLayout: 'main'}));
+server.set('view engine', 'handlebars');
+
+server.use(express.static(path.join(__dirname, 'public')));
+
+server.get('/', function(request, response, next) {
+  var templateArgs = {
+	  title: "Title",
+  };
+  response.render('mainPage', templateArgs);
+});
+
+server.get('/public/*', function(request, response, next) {
+  var filePath = '.' + request.url;
+  var extname = ''
+  extname = path.extname(filePath);
+  var contentType = 'text/html';
+
+  switch (extname) {
+	  case '.js':
+		  contentType = 'text/javascript';
+		  break;
+	  case '.css':
+		  contentType = 'text/css';
+		  break;
+	  case '.json':
+		  contentType = 'application/json';
+		  break;
+	  case '.png':
+		  contentType = 'image/png';
+		  break;      
+	  case '.jpg':
+		  contentType = 'image/jpg';
+		  break;
+	  case '.wav':
+		  contentType = 'audio/wav';
+		  break;
+  }
+
+  fs.readFile(filePath, function(error, content) {
+	  if (error) {
+		  if(error.code == 'ENOENT'){
+			  response.render('404Page');
+		  }
+		  else {
+			  responseponse.writeHead(500);
+			  responseponse.end('Sorry, check with the site admin for error: '+error.code+' ..\n');
+			  responseponse.end(); 
+		  }
+	  }
+	  else {
+		  response.writeHead(200, { 'Content-Type': contentType });
+		  response.end(content, 'utf-8');
+	  }
+  });
+});
+
+server.get('/assets/*', function(request, response, next) {
+  var filePath = '.' + request.url;
+  var extname = ''
+  extname = path.extname(filePath);
+  var contentType = 'text/html';
+  switch (extname) {
+    case '.jpg':
+	  contentType = 'image/jpg';
+	  break;
+  }
+  fs.readFile(filePath, function(error, content) {
+	if (error) {
+      response.render('404Page');
+	} else {
+      response.writeHead(200, { 'Content-Type': contentType });
+	  response.end(content, 'utf-8');
+	}
+  });
+});
+
+server.get('/:page/:index', function(request, response, next) {
+  console.log("page request", request.params.page);
+  console.log("index request", request.params.index);
+});
+
+server.get('*', function(request, response) {
+  response.render('404Page');
+});
+
+server.listen(port, function() {
+  console.log("Server running on port ", port);
+});
