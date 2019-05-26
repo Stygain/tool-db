f508976b5a9ccc86435fd0ad508d3f8267b1f330
Adam Barton <adamb6102@gmail.com>
2019-05-25 20:33:23 -0700

    Added database integration, when registering an account and logging into an account. Also added cookies on successful login.

--- server.js
+++ server.js
@@ -5,7 +5,10 @@ var http = require('http');
 var path = require('path');
 var exphbs = require('express-handlebars');
 var hbs = require('handlebars');
-// var bootstrap = require('bootstrap');
+var util = require('util');
+var sql = require('mysql');
+//var sql = require('mssql');
+var bcrypt = require('bcrypt');
 
 var port = process.env.PORT || 3000;
 
@@ -13,6 +16,18 @@ server.engine('handlebars', exphbs({defaultLayout: 'main'}));
 server.set('view engine', 'handlebars');
 
 server.use(express.static(path.join(__dirname, 'public')));
+
+server.use(express.urlencoded({extended: true}));
+server.use(express.json());
+
+var connection = sql.createConnection({
+	host: "classmysql.engr.oregonstate.edu",
+	user: "cs340_bartonad",
+	password: "potato",
+	database: "cs340_bartonad",
+});
+
+connection.connect();
 
 server.get('/', function(request, response, next) {
   var templateArgs = {
@@ -156,6 +171,52 @@ server.get('/:page/:index', function(request, response, next) {
 
 server.get('*', function(request, response) {
   response.render('404Page');
+});
+
+server.post('/login', function(request, response) {
+	console.log("Request: " + request.body.username);
+
+	// Do some database stuff
+	var query = sql.format('SELECT password FROM User WHERE email = ?', [request.body.username]);
+	console.log(query);
+	connection.query(query, function (error, results, fields) {
+		if (error) throw error;
+		console.log('The results: ', results);
+		console.log("PASS?: " + results[0].password);
+		bcrypt.compare(request.body.password, results[0].password, function(err, res) {
+			// If they are authorized, set a cookie
+			if (res == true) {
+				console.log("IT IS TRUE");
+				// Now figure out what value to use for the cookie
+				response.cookie('cookiename', 'cookievalue', { maxAge: 900000, httpOnly: true });
+				response.write('hi');
+			}
+		});
+	});
+});
+
+server.post('/register', function(request, response) {
+	console.log("Request username: " + request.body.username);
+	console.log("Request password: " + request.body.password);
+	// Do some database stuff
+
+	const saltRounds = 10;
+	var password = request.body.password;
+	var pass_hash;
+
+	bcrypt.hash(password, saltRounds, function(err, hash) {
+		// Store hash in your password DB.
+		console.log("HASH: " + hash);
+		pass_hash = hash;
+
+		var post  = {email: request.body.username, password: pass_hash};
+		var query = connection.query('INSERT INTO User SET ?', post, function (error, results, fields) {
+			if (error) throw error;
+			// Neat!
+		});
+	});
+
+	response.write('hi');
 });
 
 server.listen(port, function() {
