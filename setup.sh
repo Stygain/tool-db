0b1246ab3624ff2326ec854c46df637510834f92
Adam Barton <adamb6102@gmail.com>
2019-05-26 17:52:28 -0700

    Added setup script to run to get dependencies to run with.

---
+++ setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+sudo apt-get install npm
+
+npm install express
+npm install express-handlebars
+npm install express-session
+npm install handlebars
+npm install mysql
+npm install bcrypt
+npm install cookie-parser
