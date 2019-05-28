1e1eeb7d20c7fe51db2227ddddacf47ccca9f27c
Adam Barton <adamb6102@gmail.com>
2019-05-28 10:42:19 -0700

    Updated setup script to actually install the correct packages.

--- setup.sh
+++ setup.sh
@@ -1,11 +1,15 @@
 #!/bin/bash
 
-sudo apt-get install npm
+sudo apt install curl
 
+curl -sL https://deb.nodesource.com/setup_8.x | sudo bash -
+
+sudo apt-get install nodejs
+
+npm install handlebars
 npm install express
 npm install express-handlebars
 npm install express-session
-npm install handlebars
 npm install mysql
 npm install bcrypt
 npm install cookie-parser
