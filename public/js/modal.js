216c41d6aa29937d48064e8efd67b0da65cb94a3
Adam Barton <adamb6102@gmail.com>
2019-06-03 20:37:22 -0700

    Fixed tabbing.

--- public/js/modal.js
+++ public/js/modal.js
@@ -23,3 +23,59 @@ window.onclick = function(event) {
 		modal.style.display = "none";
 	}
 }
+
+function submitAction(button) {
+	// AJAX Request
+	xhr = new XMLHttpRequest();
+
+	var urlEncodedData = "";
+	var urlEncodedDataPairs = {};
+	var field;
+	var inputList = document.querySelectorAll("input");
+
+	if (!xhr) {
+		alert("Cannot create http request.")
+		return false;
+	}
+
+	if (button == 'buildings') {
+		console.log("Making post to buildings");
+		xhr.open('POST', '/buildings');
+	} else if (button == 'locations') {
+		console.log("Making post to locations");
+		xhr.open('POST', '/locations');
+	} else if (button == 'tools') {
+		console.log("Making post to tools");
+		xhr.open('POST', '/tools');
+	} else if (button == 'maintainers') {
+		console.log("Making post to maintainers");
+		xhr.open('POST', '/maintainers');
+	} else {
+		console.log("Making post to NOTHING");
+		for (field = 0; field < inputList.length; field++) {
+			urlEncodedDataPairs[inputList[field].name] = inputList[field].value;
+		}
+	}
+
+	//var modal = document.getElementById("status-container");
+	//xhr.onreadystatechange = function() {
+	//    console.log("STATUS: " + this.status);
+	//    if (this.status == 200) {
+	//	document.getElementById("status").innerHTML = "Success!";
+	//    } else {
+	//	document.getElementById("status").innerHTML = "Failed to authenticate user!";
+	//    }
+	//    modal.style.display = "block";
+
+	//    // Register a timeout to make it go away in a couple of seconds
+	//    setTimeout(function(modal) {
+	//		modal.style.display = "none";
+	//		window.location.replace("localhost:3000/");
+	//		location.reload();
+	//    }, 900, modal);
+	//};
+	//xhr.setRequestHeader('Content-Type', 'application/json')
+
+	//console.log(JSON.stringify(urlEncodedDataPairs));
+	//xhr.send(JSON.stringify(urlEncodedDataPairs));
+}
