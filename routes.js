743e2f2d7d79ac81148ae0fd57e1d37894eb712f
Adam Barton <adamb6102@gmail.com>
2019-06-13 21:11:16 -0700

    Moved post request handling out of server.js and into the routes.js file.

--- routes.js
+++ routes.js
@@ -1,14 +1,18 @@
 // Configuration of all the server routes
 
+var sql = require('mysql');
+
 var renderer = require('./handlebars/renderer.js');
 var auth = require('./auth.js');
+var db = require('./db/db.js');
 var query = require('./db/queries.js');
 
 module.exports = {
-	RouteHandler: routeHandler,
+	GET: getRouteHandler,
+	POST: postRouteHandler,
 };
 
-var pageConfig = {
+var getConfig = {
 	'^/login$': renderLogin,
 	'^/register$': renderRegister,
 	'^/buildings$': renderBuildings,
@@ -19,6 +23,21 @@ var pageConfig = {
 	'^/locations_and_tools$': renderLocationsAndTools,
 	'^/buildings_and_locations$': renderBuildingsAndLocations,
 	'^/logout$': handleLogout,
+};
+
+var postConfig = {
+	'^/login$': postLogin,
+	'^/register$': postRegister,
+	'^/buildings$': postBuildings,
+	'^/buildingDelete$': postBuildingDelete,
+	'^/locations$': postLocations,
+	'^/locationDelete$': postLocationDelete,
+	'^/tools$': postTools,
+	'^/toolDelete$': postToolDelete,
+	'^/contains$': postContains,// Remove this page eventually
+	'^/containDelete$': postContainDelete,// Remove this page eventually
+	'^/maintainers$': postMaintainers,
+	'^/maintainerDelete$': postMaintainerDelete,
 };
 
 /* ********************
@@ -189,19 +208,314 @@ function renderBuildingsAndLocations(request, response) {
 	});
 };
 
-function routeHandler(route, request, response) {
+/* ********************
+ * Handle get requests
+******************** */
+function getRouteHandler(route, request, response) {
 	console.log("ROUTE: " + route);
 	if (route == '/') {
 		renderer.RenderHomePage(request, response);
+		return;
 	}
-	for (var routeRegex in pageConfig) {
+	for (var routeRegex in getConfig) {
 		var regex = new RegExp(routeRegex, "g");
 		var results = route.match(regex);
-		console.log("RESULTS against " + regex);
-		console.log(results);
 		if (route.match(regex) != null) {
 			console.log("MATCHED: " + regex);
-			pageConfig[routeRegex](request, response);
+			getConfig[routeRegex](request, response);
+			return;
+		}
+	}
+	renderer.Render404Page(request, response);
+};
+
+
+
+
+
+
+/* ********************
+ * Handle post requests for /login
+******************** */
+function postLogin(request, response) {
+	// Do some database stuff
+	var query = sql.format('SELECT password FROM User WHERE email = ?', [request.body.username]);
+	db.Query(query, function(status, results) {
+		if (!status) {
+			console.log("ERROR: " + results);
+			response.status(401).end();
+			return;
+		}
+		if (results.length == 0) {
+			response.status(401).end();
+			return;
+		}
+		bcrypt.compare(request.body.password, results[0].password, function(err, res) {
+			// If they are authorized, set a cookie
+			if (res == true) {
+				// Now figure out what value to use for the cookie
+				var cookieName = "site_auth";
+				var toHash = request.body.username + request.body.password;
+				bcrypt.hash(toHash, saltRounds, function(err, hash) {
+					var cookieValue = hash;
+					response.cookie(cookieName, "somerandonstuffs", { maxAge: 900000, httpOnly: true });
+					response.status(200).end();
+				});
+			} else {
+			    response.status(401).end();
+			}
+		});
+	});
+};
+
+/* ********************
+ * Handle post requests for /register
+******************** */
+function postRegister(request, response) {
+	if (request.body.password != request.body.reenterPassword) {
+		console.log("User passwords did not match");
+		response.status(401).end();
+		return;
+	}
+
+	var password = request.body.password;
+	var passHash;
+
+	bcrypt.hash(password, saltRounds, function(err, hash) {
+		// Store hash in your password DB.
+		console.log("HASH: " + hash);
+		passHash = hash;
+
+		var post  = {email: request.body.username, password: passHash};
+		var query = sql.format('INSERT INTO User SET ?', post);
+		db.Query(query, function(status, results) {
+			if (!status) {
+				console.log("ERROR: " + results);
+				response.status(401).end();
+				return;
+			} else {
+			    response.status(200).end();
+			}
+		});
+	});
+};
+
+/* ********************
+ * Handle post requests for /buildings
+******************** */
+function postBuildings(request, response) {
+	//console.log("Buildings Data: ");
+	//console.log(request.body);
+	//console.log(request.body.address);
+	//console.log(request.body.name);
+	//console.log(request.body.manager);
+
+	var query = sql.format('INSERT INTO Building SET ?', request.body);
+	db.Query(query, function(status, results) {
+		if (!status) {
+			console.log("ERROR: " + results);
+			response.status(401).end();
+			return;
+		} else {
+			response.status(200).end();
+		}
+	});
+};
+
+/* ********************
+ * Handle post requests for /buildingDelete
+******************** */
+function postBuildingDelete(request, response) {
+	//console.log("Building Delete Data: ");
+	//console.log(request.body);
+
+	var query = sql.format('DELETE FROM Building WHERE address = ?', request.body.address);
+	db.Query(query, function(status, results) {
+		if (!status) {
+			console.log("ERROR: " + results);
+			response.status(401).end();
+			return;
+		} else {
+			response.status(200).end();
+		}
+	});
+};
+
+/* ********************
+ * Handle post requests for /locations
+******************** */
+function postLocations(request, response) {
+	//console.log("Locations Data: ");
+	//console.log(request.body);
+
+	var query = sql.format('INSERT INTO Location SET ?', request.body);
+	db.Query(query, function(status, results) {
+		if (!status) {
+			console.log("ERROR: " + results);
+			response.status(401).end();
+			return;
+		} else {
+			response.status(200).end();
+		}
+	});
+};
+
+/* ********************
+ * Handle post requests for /locationDelete
+******************** */
+function postLocationDelete(request, response) {
+	//console.log("Location Delete Data: ");
+	//console.log(request.body);
+
+	var query = sql.format('DELETE FROM Location WHERE ID = ?', request.body.ID);
+	db.Query(query, function(status, results) {
+		if (!status) {
+			console.log("ERROR: " + results);
+			response.status(401).end();
+			return;
+		} else {
+			response.status(200).end();
+		}
+	});
+};
+
+/* ********************
+ * Handle post requests for /tools
+******************** */
+function postTools(request, response) {
+	//console.log("Tools Data: ");
+	//console.log(request.body);
+
+	var queryData = {TID: request.body.tid, name: request.body.name, "business name": request.body.maintainer};
+	var query = sql.format('INSERT INTO Tool SET ?', queryData);
+	db.Query(query, function(status, results) {
+		if (!status) {
+			console.log("ERROR: " + results);
+			response.status(401).end();
+			return;
+		}
+	});
+	queryData = {ID: request.body.location, TID: request.body.tid};
+	query = sql.format('INSERT INTO Contains SET ?', queryData);
+	db.Query(query, function(status, results) {
+		if (!status) {
+			console.log("ERROR: " + results);
+			response.status(401).end();
+			return;
+		} else {
+			response.status(200).end();
+		}
+	});
+};
+
+/* ********************
+ * Handle post requests for /toolDelete
+******************** */
+function postToolDelete(request, response) {
+	//console.log("Tool Delete Data: ");
+	//console.log(request.body);
+
+	var query = sql.format('DELETE FROM Tool WHERE TID = ?', request.body.TID);
+	db.Query(query, function(status, results) {
+		if (!status) {
+			console.log("ERROR: " + results);
+			response.status(401).end();
+			return;
+		} else {
+			response.status(200).end();
+		}
+	});
+};
+
+/* ********************
+ * Handle post requests for /contains
+ ******************** */
+function postContains(request, response) {
+	console.log("Contains Data: ");
+	console.log(request.body);
+
+	var queryData = {ID: request.body.lid, TID: request.body.tid};
+	var query = sql.format('INSERT INTO Contains SET ?', queryData);
+	db.Query(query, function(status, results) {
+		if (!status) {
+			console.log("ERROR: " + results);
+			response.status(401).end();
+			return;
+		} else {
+			response.status(200).end();
+		}
+	});
+};
+
+/* ********************
+ * Handle post requests for /containsDelete
+ ******************** */
+function postContainDelete(request, response) {
+	console.log("Contains Delete Data: ");
+	console.log(request.body);
+
+	var query = sql.format('DELETE FROM Contains WHERE TID = ? AND ID = ?', [request.body.TID, request.body.ID]);
+	db.Query(query, function(status, results) {
+		if (!status) {
+			console.log("ERROR: " + results);
+			response.status(401).end();
+			return;
+		} else {
+			response.status(200).end();
+		}
+	});
+};
+
+/* ********************
+ * Handle post requests for /maintainers
+******************** */
+function postMaintainers(request, response) {
+	//console.log("Maintainers Data: ");
+	//console.log(request.body);
+
+	var queryData = {name: request.body.name, phone: request.body.phone, email: request.body.email};
+	var query = sql.format('INSERT INTO `Maintenance Company` SET ?', queryData);
+	db.Query(query, function(status, results) {
+		if (!status) {
+			console.log("ERROR: " + results);
+			response.status(401).end();
+			return;
+		} else {
+			response.status(200).end();
+		}
+	});
+};
+
+/* ********************
+ * Handle post requests for /maintainerDelete
+******************** */
+function postMaintainerDelete(request, response) {
+	//console.log("Maintainer Delete Data: ");
+	//console.log(request.body);
+
+	var query = sql.format('DELETE FROM `Maintenance Company` WHERE name = ?', request.body.name);
+	db.Query(query, function(status, results) {
+		if (!status) {
+			console.log("ERROR: " + results);
+			response.status(401).end();
+			return;
+		} else {
+			response.status(200).end();
+		}
+	});
+};
+
+/* ********************
+ * Handle post requests
+******************** */
+function postRouteHandler(route, request, response) {
+	console.log("ROUTE: " + route);
+	for (var routeRegex in postConfig) {
+		var regex = new RegExp(routeRegex, "g");
+		var results = route.match(regex);
+		if (route.match(regex) != null) {
+			console.log("MATCHED: " + regex);
+			postConfig[routeRegex](request, response);
 			return;
 		}
 	}
